df8e37407f938e63251faf887b4c499f9e1bf9b1
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-02-28 12:09:32 -0800

    Setup hero constructor

--- components/hero/hero.ts
+++ components/hero/hero.ts
@@ -4,15 +4,19 @@ import { DistanceFunction } from "../../node_modules/toolbox-v2/src/toolbox/comp
 
 class Hero {
   private scrollEffect_: ScrollEffect = null;
-  constructor() {}
+  private scale_: number;
+  constructor(scale: number) {
+    this.scale_ = scale;
+  }
   startScrollEffect(): void {
+    const translateY = ((this.scale_ - 1) / 2) * -100;
     this.scrollEffect_ = new ScrollEffect(
       <HTMLElement>document.querySelector(".hero__background"),
       {
         effects: [
           new Tween([
             [0, "transform: translateY(0) scale(1)"],
-            [1, "transform: translateY(-5%) scale(1.1)"]
+            [1, `transform: translateY(${translateY}%) scale(${this.scale_})`]
           ])
         ],
         getDistanceFunction: DistanceFunction.DOCUMENT_SCROLL,
--- global/main.ts
+++ global/main.ts
@@ -2,4 +2,4 @@ import { Nav } from "../components/nav/nav";
 import { Hero } from "../components/hero/hero";
 
 new Nav().startScrollWatcher();
-new Hero().startScrollEffect();
+new Hero(1.1).startScrollEffect();
