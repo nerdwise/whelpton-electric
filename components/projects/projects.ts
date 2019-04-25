cb92ddbc5f7c21f1c6f6599b7aad899c710bfc0f
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-04-25 12:40:58 -0700

    Remove uses of onclick

--- components/projects/projects.ts
+++ components/projects/projects.ts
@@ -1,44 +1,44 @@
-import { loadImage } from "../../node_modules/toolbox-v2/src/toolbox/utils/loading/load-image";
+import { loadImage } from '../../node_modules/toolbox-v2/src/toolbox/utils/loading/load-image';
 
 class Projects {
   images: HTMLElement[];
   modal: HTMLElement;
   constructor() {
-    this.images = Array.from(document.querySelectorAll(".gallery__image"));
-    this.modal = document.querySelector(".modal");
+    this.images = Array.from(document.querySelectorAll('.gallery__image'));
+    this.modal = document.querySelector('.modal');
   }
 
   openModal(): void {
     this.images.forEach(image => {
-      image.onclick = () => {
-        this.modal.classList.add("display");
+      image.addEventListener('click', () => {
+        this.modal.classList.add('display');
         const modalImage: HTMLImageElement = document.querySelector(
-          ".modal__image--" + image.dataset.target
+          '.modal__image--' + image.dataset.target
         );
         const modalImageSrc: string = `/static/images/projects/projects${
           image.dataset.target
         }-min.jpg`;
         loadImage(modalImageSrc).then(() => {
           modalImage.src = modalImageSrc;
-          modalImage.classList.add("display-image");
+          modalImage.classList.add('display-image');
           this.closeModal(modalImage);
         });
-      };
+      });
     });
   }
 
   closeModal(image: Element): void {
-    window.onclick = event => {
+    window.addEventListener('click', event => {
       if (event.target == this.modal) {
-        this.modal.classList.remove("display");
-        image.classList.remove("display-image");
+        this.modal.classList.remove('display');
+        image.classList.remove('display-image');
       }
-    };
-    const x: HTMLElement = document.querySelector(".modal__x");
-    x.onclick = event => {
-      this.modal.classList.remove("display");
-      image.classList.remove("display-image");
-    };
+    });
+    const x: HTMLElement = document.querySelector('.modal__x');
+    x.addEventListener('click', event => {
+      this.modal.classList.remove('display');
+      image.classList.remove('display-image');
+    });
   }
 
   init(): void {
